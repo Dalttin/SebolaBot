189e8a2448c672f599180b7be660ae432a84c034
Dalttin <boulevard916@gmail.com>
2021-02-14 19:13:02 -0300

    First commit

---
+++ SebolaDLL/src/bruxinhomenu.js
@@ -0,0 +1,12 @@
+const sebolamenu = (prefix, pushname) => {
+    return `◪ *Comandos Criador*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.sebolamenu = sebola
